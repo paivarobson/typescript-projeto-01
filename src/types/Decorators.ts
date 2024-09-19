0c18f45bc7222d6a37bf1abe704a7255f1932c87
Robson <paivawork@hotmail.com>
2024-09-19 18:14:16 -0300

    Adicionado validação do depósito usando Decorators

--- src/types/Decorators.ts
+++ src/types/Decorators.ts
@@ -20,3 +20,20 @@ export function ValidaDebito(
 
   return descriptor;
 }
+
+export function ValidaDeposito(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  descriptor.value = function (valorDoDeposito: number) {
+    if (valorDoDeposito <= 0) {
+      throw new Error("O valor a ser depositado deve ser maior do que zero!");
+    }
+
+    return originalMethod.apply(this, [valorDoDeposito]);
+  };
+
+  return descriptor;
+}
